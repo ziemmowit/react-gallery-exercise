e184749c7d312c2bf3c9bb7dd5c857611f85895b
Ziemowit Radogostowicz <ziemowit@zwr.co>
2018-10-10 11:05:12 +0200

    Getting and rendering data from shibe.online

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,33 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from 'axios';
 
 class PhotoList extends React.Component {
-  render () {
-    let list = [
-      "https://cdn.shibe.online/shibes/2058a42728365ca429277095a98a540c1d3dfbbf.jpg",
-      "https://cdn.shibe.online/shibes/916dcb73e65259a0ffe9adf0a9be3e3a14981825.jpg",
-      "https://cdn.shibe.online/shibes/5e824c7b11a9f417af76c09978f087d0606c2691.jpg",
-      "https://cdn.shibe.online/shibes/6bc2fe0e0e91bda19b5986e893c20f9e3cf2231e.jpg",
-      "https://cdn.shibe.online/shibes/916dcb73e65259a0ffe9adf0a9be3e3a14981825.jpg",
-      "https://cdn.shibe.online/shibes/2058a42728365ca429277095a98a540c1d3dfbbf.jpg",
-      "https://cdn.shibe.online/shibes/916dcb73e65259a0ffe9adf0a9be3e3a14981825.jpg"
-    ]
-    return (
+  state = {photoList: []}
 
+  componentDidMount() {
+    this.fetchPhotos();
+  }
+
+  fetchPhotos = () => {
+    let pNum = 16
+
+    axios({
+      url: `https://shibe.online/api/shibes?count=${pNum}`,
+      method: 'GET',
+    }).then(response => {
+      this.setState({
+        photoList: response.data
+      })
+    }, error => {
+      this.setState({
+        photoList: []
+      })
+    })
+  }
+
+  extractId = (str) => {
+    const arr = str.split("/");
+    return arr[arr.length - 1].split(".")[0];
+  }
+
+  render () {
+    let {photoList} = this.state;
+
+    return (
       <div id="photo-list">
         <div className="row">
-          {
-            list.map((link, i) => (
-              <div className="col-3">
-                <div className="photo-item">
-                  <img className="img-thumbnail" src={link} alt="..." />
-                  <button href="#" className="add-to-favourites btn btn-link"><i className="fas fa-star"></i></button>
+          { photoList.length > 0 ?
+              photoList.map((link, i) => (
+                <div key={this.extractId(link)} className="col-3">
+                  <div className="photo-item">
+                    <img className="img-thumbnail" src={link} alt={this.extractId(link)} />
+                    <button href="#" className="add-to-favourites btn btn-link"><i className="fas fa-star"></i></button>
+                  </div>
                 </div>
-              </div>
-            ))
+              ))
+            :
+            (<p>Błąd ładowania danych</p>)
           }
         </div>
       </div>
+
     )
   }
 }
