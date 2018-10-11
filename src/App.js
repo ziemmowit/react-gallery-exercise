c1f5b33595a7efae3c7f6f71a2853027c9913d72
Ziemowit Radogostowicz <ziemowit@zwr.co>
2018-10-11 21:20:45 +0200

    Header as a separate component, and nav item activity.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from 'axios';
 
+import Header from './components/Header';
 import Photo from './components/Photo';
 import PhotoList from './components/PhotoList';
 
@@ -51,22 +52,7 @@ class App extends React.Component {
 
     return (
       <div>
-        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Galeria</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active" onClick={this.showPhotoList}>
-                <a className="nav-link" href="#">Lista zdjęć <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item" onClick={this.showFavList}>
-                <a className="nav-link" href="#">Ulubione</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <Header showPhotoList={this.showPhotoList} showFavList={this.showFavList} currentPage={currentPage}/>
         <div className="container">
           <PhotoList photoList={photoList} favList={favList} mode={currentPage} loaded={loaded}/>
         </div>
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Header extends React.Component {
+  render () {
+    return (
+      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Galeria</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className={this.props.currentPage === 'photoList' ? 'nav-item active' : 'nav-item' } onClick={this.props.showPhotoList}>
+              <a className="nav-link" href="#">Lista zdjęć <span className="sr-only">(current)</span></a>
+            </li>
+            <li className={this.props.currentPage === 'favList' ? 'nav-item active' : 'nav-item' } onClick={this.props.showFavList}>
+              <a className="nav-link" href="#">Ulubione</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header;
